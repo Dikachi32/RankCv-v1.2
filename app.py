9e0cc5f086f216630ab516f54bd8a5fd0b775859
Dikachi Baron <francisonyedikachiagwu@gmail.com>
2026-05-26 07:16:25 -0700

    Change app.run to disable debug mode

--- app.py
+++ app.py
@@ -1231,8 +1231,8 @@ def too_large(e):
 # =============================================================================
 # STARTUP
 # =============================================================================
+
 if __name__ == '__main__':
-    import os
     with app.app_context():
         db.create_all()
         print("\n" + "="*55)
@@ -1244,4 +1244,4 @@ if __name__ == '__main__':
         print("-"*55)
     
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
